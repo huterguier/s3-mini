a2a4408388d60565a584e43224b9260107973a83
huterguier <huterguier@gmail.com>
2025-09-01 17:44:50 +0200

    added tqdm progress bar

--- s3-mini/boot.py
+++ s3-mini/boot.py
@@ -1,17 +1,32 @@
 from pynput import keyboard
 import pygame
 import threading
+import tqdm
 
 pygame.mixer.init()
-sound = pygame.mixer.Sound("media/samsung.mp3")
+sound = pygame.mixer.Sound("media/moemax.mp3")
+
+n = 10000
+counter = 0
+pbar = None
 
 def play_sound():
-    sound.play()
+    global counter, pbar
+    if counter % n == 0:
+        sound.play()
+        if pbar is not None:
+            pbar.reset()
+    if pbar is not None:
+        pbar.update(1)
+    counter += 1
+
 
 def on_press(key):
     threading.Thread(target=play_sound).start()
 
 def main():
+    global pbar
+    pbar = tqdm.tqdm(total=n)
     with keyboard.Listener(on_press=on_press) as listener:
         listener.join()
 
